d173d47831e6fbea7a4eeceda57f3c61f4660140
KevinPaxman <KevinPaxman@1480812.no-reply.drupal.org>
2014-09-23 21:41:33 +0200

    Issue #1945230 by kpaxman, johnv: Fixed JavaScript for multi-block widget.

--- office_hours.js
+++ office_hours.js
@@ -1,15 +1,42 @@
 (function ($) {
-Drupal.behaviors.office_hours = {
-  attach: function(context,settings) {
-  $(".oh-hide", context).parent().hide();
-  $(".oh-add-more-link", context).each(function (i) {
-   $(this).parent().children("div.office-hours-block").hide();
-    })
-    .click(function () {
-    $(this).hide();
-    $(this).parent().children("div.office-hours-block").fadeIn("slow");
-    return false;
-  });
-}
-};
-})(jQuery);
+  Drupal.behaviors.office_hours = {
+    attach: function(context,settings) {
+      $(".oh-hide", context).parent().hide();
+      $(".oh-add-more-link", context).each(function (i) {
+        $(this).parent().children("div.office-hours-block").hide();
+        // If the previous row has an "Add more hours" link, and the office-hours-block is hidden, don't show this line.
+        $this_tr = $(this).closest("tr");
+        if ($this_tr.prev().find(".oh-add-more-link").length && !$this_tr.prev().find("div.office-hours-block:visible").length) {
+          $this_tr.hide();
+        }
+      })
+      .click(show_upon_click);
+      fix_striping();
+
+      // Function to show an office-hours-block, when user clicks "Add more hours".
+      function show_upon_click() {
+        $(this).hide();
+        $(this).parent().children("div.office-hours-block").fadeIn("slow");
+        // If the next for has an "add more hours" link, show it.
+        $next_tr = $(this).closest("tr").next();
+        if ($next_tr.find(".oh-add-more-link").length) {
+          $next_tr.show();
+        }
+        fix_striping();
+        return false;
+      };
+
+      // Function to traverse visible rows and apply even/odd classes.
+      function fix_striping() {
+        $('tbody tr:visible', context).each(function (i) {
+          if (i % 2 == 0) {
+            $(this).removeClass('odd').addClass('even');
+          } else {
+            $(this).removeClass('even').addClass('odd');
+          }
+        });
+      }
+
+    }
+  };
+ })(jQuery);
