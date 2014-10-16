767b18f6ce8bbafaefe744e2fffb458350a7c9b6
johnv <john.voskuilen@sapito.nl>
2014-10-16 15:13:32 +0200

    Changed use  including minutes in widget/elements.

--- office_hours.js
+++ office_hours.js
@@ -37,6 +37,28 @@
         return false;
       };
 
+      $('.oh-apply-link-0').bind('click', function (e) {
+        // Get field name.
+        var $field_name = $(this).closest('.field-type-office-hours').attr('id');
+
+        // Get field name with language and delta.
+        $this_field_classes = $(this).parent().parent().children().closest('div').attr('class').split(' ');
+        $($this_field_classes).each(function (i, val){
+          if (/form-item-field/i.test(val)) {
+            $this_field_name = val.replace('form-item-','').replace('-starthours-hours','');
+          }
+        });
+
+        $('select[id^='+$field_name+'][id$="starthours-hours"]').val($('#edit-'+$this_field_name+'-starthours-hours').val());
+        $('select[id^='+$field_name+'][id$="starthours-minutes"]').val($('#edit-'+$this_field_name+'-starthours-minutes').val());
+        $('select[id^='+$field_name+'][id$="starthours-ampm"]').val($('#edit-'+$this_field_name+'-starthours-ampm').val());
+        $('select[id^='+$field_name+'][id$="endhours-hours"]').val($('#edit-'+$this_field_name+'-endhours-hours').val());
+        $('select[id^='+$field_name+'][id$="endhours-minutes"]').val($('#edit-'+$this_field_name+'-endhours-minutes').val());
+        $('select[id^='+$field_name+'][id$="endhours-ampm"]').val($('#edit-'+$this_field_name+'-endhours-ampm').val());
+
+        e.preventDefault();
+      });
+
       // Function to traverse visible rows and apply even/odd classes.
       function fix_striping() {
         $('tbody tr:visible', context).each(function (i) {
